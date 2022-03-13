1fea6ab5e4e77896789025bc3eb9c6df4af74c38
hoshi4444 <hoshi44444444@gmail.com>
2022-03-13 21:46:28 +0900

    laravelviteを導入できたけどhotReloadできなかったので見送り

---
+++ src/resources/views/pages/Auth/Login.vue
@@ -0,0 +1,98 @@
+<template>
+    <Head title="Log in" />
+
+    <jet-authentication-card>
+        <template #logo>
+            <jet-authentication-card-logo />
+        </template>
+
+        <jet-validation-errors class="mb-4" />
+
+        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
+            {{ status }}
+        </div>
+
+        <form @submit.prevent="submit">
+            <div>
+                <jet-label for="email" value="Email" />
+                <jet-input id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autofocus />
+            </div>
+
+            <div class="mt-4">
+                <jet-label for="password" value="Password" />
+                <jet-input id="password" type="password" class="mt-1 block w-full" v-model="form.password" required autocomplete="current-password" />
+            </div>
+
+            <div class="block mt-4">
+                <label class="flex items-center">
+                    <jet-checkbox name="remember" v-model:checked="form.remember" />
+                    <span class="ml-2 text-sm text-gray-600">Remember me</span>
+                </label>
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <Link v-if="canResetPassword" :href="route('password.request')" class="underline text-sm text-gray-600 hover:text-gray-900">
+                    Forgot your password?
+                </Link>
+
+                <jet-button class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                    Log in
+                </jet-button>
+            </div>
+        </form>
+    </jet-authentication-card>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue'
+    import JetAuthenticationCard from '@/views/components/AuthenticationCard.vue'
+    import JetAuthenticationCardLogo from '@/views/components/AuthenticationCardLogo.vue'
+    import JetButton from '@/views/components/Button.vue'
+    import JetInput from '@/views/components/Input.vue'
+    import JetCheckbox from '@/views/components/Checkbox.vue'
+    import JetLabel from '@/views/components/Label.vue'
+    import JetValidationErrors from '@/views/components/ValidationErrors.vue'
+    import { Head, Link } from '@inertiajs/inertia-vue3';
+
+    export default defineComponent({
+        components: {
+            Head,
+            JetAuthenticationCard,
+            JetAuthenticationCardLogo,
+            JetButton,
+            JetInput,
+            JetCheckbox,
+            JetLabel,
+            JetValidationErrors,
+            Link,
+        },
+
+        props: {
+            canResetPassword: Boolean,
+            status: String
+        },
+
+        data() {
+            return {
+                form: this.$inertia.form({
+                    email: '',
+                    password: '',
+                    remember: false
+                })
+            }
+        },
+
+        methods: {
+            submit() {
+                this.form
+                    .transform(data => ({
+                        ... data,
+                        remember: this.form.remember ? 'on' : ''
+                    }))
+                    .post(this.route('login'), {
+                        onFinish: () => this.form.reset('password'),
+                    })
+            }
+        }
+    })
+</script>
