d1337fc22fe30c78c72498da70bccb0e160a8c5f
hoshi4444 <hoshi44444444@gmail.com>
2022-03-16 03:02:41 +0900

    打刻時刻登録処理を追加

--- src/resources/js/Pages/Work/WorkCard.vue
+++ src/resources/js/Pages/Work/WorkCard.vue
@@ -6,11 +6,8 @@
                 <p>{{ day }}</p>
             </div>
             <div class="left-side-info flex space-x-2">
-                <work-stamp-red-button
-                    :disabled="newStampIdx == -1"
-                    @click="postNewStamp()"
-                >Save New Stamp!</work-stamp-red-button>
                 <work-stamp-blue-button @click="createNewStamp()">Push New Stamp!!</work-stamp-blue-button>
+                <work-stamp-red-button :disabled="!newStamp" @click="postNewStamp()">Save New Stamp!</work-stamp-red-button>
             </div>
         </div>
 
@@ -22,10 +19,11 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import WorkSequence from './WorkSequence.vue';
 import WorkStampBlueButton from './WorkStampBlueButton.vue';
 import WorkStampRedButton from './WorkStampRedButton.vue';
+import { useForm } from '@inertiajs/inertia-vue3';
 import { DateTime } from "luxon";
 
 interface Props {
@@ -35,23 +33,41 @@ interface Props {
 
 const props = defineProps<Props>();
 const work = computed(() => props.work);
-const newStampIdx = computed(() => work.value.work_stamps.findIndex(stamp => stamp.id == 0));
+const newStamp = ref<WorkStamp | null>(null)
+
+const form = useForm({
+    newStamp: <WorkStamp>{},
+});
 
 // 現在時刻のスタンプを追加
 // TODO: 選択した日付の時刻を入力するため、午前0時移行だと失敗する
 // TODO: 午前0時になったらリロードする処理が必要
 function createNewStamp() {
-    const newStamp: WorkStamp = {
+    newStamp.value = {
         id: 0,
+        work_id: work.value.id,
         stamp_at: DateTime.now().toFormat('H:m:s'),
         created_at: "",
         updated_at: "",
     };
 
-    if (newStampIdx.value != -1) {
-        work.value.work_stamps.splice(newStampIdx.value, 1);
+    const newStampIdx = work.value.work_stamps.findIndex(stamp => stamp.id == 0);
+    if (newStampIdx != -1) {
+        work.value.work_stamps.splice(newStampIdx, 1);
     }
 
-    work.value.work_stamps.push(newStamp);
+    work.value.work_stamps.push(newStamp.value);
+}
+
+// 打刻
+function postNewStamp() {
+    form.newStamp = newStamp.value!
+
+    form.post('/work_stamps', {
+        onSuccess: () => newStamp.value = null,
+        onError: () => {
+            alert("エラーが発生しました");
+        }
+    });
 }
 </script>
