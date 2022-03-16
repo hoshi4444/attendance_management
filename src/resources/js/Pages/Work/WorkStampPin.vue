210ce7ad01bbe4e1f019432554fab3bf56916f5e
hoshi4444 <hoshi44444444@gmail.com>
2022-03-17 00:50:35 +0900

    スタンプの動きと連動して表示時間を変化

--- src/resources/js/Pages/Work/WorkStampPin.vue
+++ src/resources/js/Pages/Work/WorkStampPin.vue
@@ -1,10 +1,15 @@
 <!-- 打刻時刻のピン -->
 <template>
     <div
-        v-if="workStampAt"
+        ref="stampBoardElm"
         class="stamp-board"
         :class="{ 'select-stamp-board': isSelected }"
+        :style="`left: ${setLeftPosition}%`"
         @click="selectStamp()"
+        @mousedown.prevent="mouseDown()"
+        @mouseup.prevent="mouseUp()"
+        @mouseout="mouseUp()"
+        @mousemove.prevent="moveOnHorizon($event)"
     >
         <div class="w-fit">
             <p class="w-full block">{{ stampCaption }}</p>
@@ -13,16 +18,15 @@
     </div>
 </template>
 
-<script setup lang="ts">
-import { computed } from 'vue';
+<script lang="ts" setup>
+import { computed, ref, watchEffect } from 'vue';
 
 interface Props {
     workStamp: WorkStamp,
     workStampIdx: number,
     workStampsLen: number,
-    hourScale: number,
-    fifteenMinScale: number
     selectedStamp: WorkStamp | null
+    sequenceElm: HTMLDivElement | null
 }
 interface Emits {
     (e: "selectStamp", workStamp: WorkStamp): void
@@ -31,10 +35,25 @@ const props = defineProps<Props>();
 const emits = defineEmits<Emits>();
 
 const workStamp = computed(() => props.workStamp);
-const hourScale = computed(() => props.hourScale);
-const fifteenMinScale = computed(() => props.fifteenMinScale);
+const workStampAt = ref(props.workStamp.stamp_at);
 const selectedStamp = computed(() => props.selectedStamp);
 const isSelected = computed(() => selectedStamp.value && selectedStamp.value.id == workStamp.value.id);
+const sequenceElm = computed(() => props.sequenceElm);
+const oneDateMin = Number(24) * Number(60);
+const isMouseDown = ref<Boolean>(false);
+const stampBoardElm = ref<HTMLDivElement | null>(null);
+
+// 時刻に合わせて左辺の位置を設定する
+const setLeftPosition = computed(() => {
+    // スタンプ時刻が1日の全分数:1440分の内、何%か求める
+    // 例:) 12:35 => 755 / 1440 = 52%
+    const [hour, min, sec] = [...workStamp.value.stamp_at.split(":").map(timeStr => Number(timeStr))];
+    const workStampAtToMin = hour * Number(60) + min;
+    const stampAtPercentage = Math.floor((workStampAtToMin / oneDateMin) * 100);
+
+    return stampAtPercentage;
+});
+
 
 // 当日何番目のスタンプかによって分岐するキャプション
 const stampCaption = computed(() => {
@@ -57,28 +76,74 @@ const stampCaption = computed(() => {
     return "Break!";
 });
 
-// 渡された時間が近かったら時刻を表示する
-const workStampAt = computed(() => {
-    // 打刻時刻の文字列を桁ごとの数値にする
-    const [hour, min, sec]: Array<number> = [...props.workStamp.stamp_at.split(':').map(timeStr => Number(timeStr))];
-
-    if (hour != hourScale.value) return null;
-
-    // 範囲内の分数なら時刻を返す
-    const isInMinutes = min >= (fifteenMinScale.value * 15) && min <= ((fifteenMinScale.value + 1) * 15);
-
-    if (isInMinutes) {
-        return props.workStamp.stamp_at;
-    } else {
-        return null;
-    }
-})
-
 // スタンプを選択する
 // WorkCardまで飛ばす
 function selectStamp() {
     console.log("select stamp pin");
     emits("selectStamp", props.workStamp);
+}
+
+function mouseDown() {
+    console.log("mouse donn");
+    isMouseDown.value = true;
+}
+
+function mouseUp() {
+    console.log("mouse up");
+    isMouseDown.value = false;
+}
+
+// initLeft ~ (100 - initLeft)間隔でスタンプを水平に動かす
+function moveOnHorizon(event: MouseEvent) {
+    watchEffect(() => {
+        if (!event) return;
+        if (!sequenceElm.value) return;
+        if (!stampBoardElm.value) return;
+        if (!isMouseDown.value) return;
+
+        // シーケンス左端からどのくらいの位置にいるのか算出
+        // 0なら左端
+        const newLeftPixel = event.x - sequenceElm.value.getBoundingClientRect().left;
+
+        // 位置をパーセンテージに直す
+        const newLeftPercentage = Math.floor((newLeftPixel / sequenceElm.value!.offsetWidth) * 100);
+
+        // leftの値を更新して動かす
+        if (newLeftPercentage < 0) {
+            stampBoardElm.value.style.left = `0%`
+        } else if (newLeftPercentage >= 100) {
+            stampBoardElm.value.style.left = `100%`
+        } else {
+            stampBoardElm.value.style.left = `${newLeftPercentage}%`
+        }
+
+        // パーセンテージを時間の文字列に直して時間を更新する
+        workStampAt.value = convertPositionPercentageToStampAtStr(newLeftPercentage);
+    });
+}
+
+// 位置のパーセンテージを逆算して時間の文字列にして返す
+function convertPositionPercentageToStampAtStr(positionPercentage: number) {
+    if (positionPercentage < 0) return "00:00";
+    if (positionPercentage >= 100) return "23:59";
+
+    // 分数に直す
+    let newMin = Math.floor(oneDateMin * (positionPercentage / 100));
+
+    // 時間と分にする
+    let newHour = Number(0);
+    while (true) {
+        if (newMin < Number(60)) {
+            break;
+        } else {
+            newMin = newMin - Number(60);
+            newHour++;
+        }
+    }
+
+    const zeroPadHour = String(newHour).padStart(2, "0");
+    const zeroPadMin = String(newMin).padStart(2, "0");
+    return `${zeroPadHour}:${zeroPadMin}`;
 }
 </script>
 <style scoped>
