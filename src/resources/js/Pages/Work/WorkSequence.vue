61c5453ac8d9ac35976da0194eb9c5a34e9d6e8f
hoshi4444 <hoshi44444444@gmail.com>
2022-03-17 00:45:33 +0900

    スタンプをネストして生成していたのを修正

--- src/resources/js/Pages/Work/WorkSequence.vue
+++ src/resources/js/Pages/Work/WorkSequence.vue
@@ -1,35 +1,37 @@
 <!-- 打刻時刻のグラフ -->
 <template>
-    <!-- 打刻時刻メモリ -->
-    <div class="flex items-end bg-blue-400 border-2 mt-6 h-14 rounded-sm">
-        <!-- 時刻メモリ -->
+    <!-- 打刻時刻ボード -->
+    <div
+        ref="sequenceElm"
+        class="flex relative items-end bg-blue-400 border-2 mt-6 h-14 rounded-sm"
+    >
+        <!-- 打刻スタンプ -->
+        <div v-if="workStamps">
+            <!-- 確実にwork_stampが存在する場合のみ表示される -->
+            <work-stamp-pin
+                v-for="(workStamp, workStampIdx) in workStamps"
+                :workStamp="workStamp"
+                :workStampIdx="workStampIdx"
+                :workStampsLen="workStamps.length"
+                :selectedStamp="selectedStamp"
+                :sequenceElm="sequenceElm"
+                @selectStamp="selectStamp"
+            />
+        </div>
+
+        <!-- 1時間間隔メモリ -->
         <div
             class="flex items-end justify-around px-2 border-l-2 cursor-pointer border-white h-8 w-1/12 relative"
             v-for="hourScale in Array(24).keys()"
             :key="hourScale"
         >
             <p class="hour-memory">{{ hourScale }}</p>
-
-            <!--  15分間隔メモリ -->
+            <!--  15間隔メモリ -->
             <div
-                class="relative border-l-2 cursor-pointer border-white h-4 w-1/12"
+                class="border-l-2 cursor-pointer border-white h-4 w-1/12"
                 v-for="fifteenMinScale in 3"
                 :key="fifteenMinScale"
-            >
-                <div v-if="workStamps">
-                    <!-- 確実にwork_stampが存在する場合のみ表示される -->
-                    <work-stamp-pin
-                        v-for="(workStamp, workStampIdx) in workStamps"
-                        :workStamp="workStamp"
-                        :workStampIdx="workStampIdx"
-                        :workStampsLen="workStamps.length"
-                        :hourScale="hourScale"
-                        :fifteenMinScale="fifteenMinScale"
-                        :selectedStamp="selectedStamp"
-                        @selectStamp="selectStamp"
-                    />
-                </div>
-            </div>
+            ></div>
         </div>
     </div>
 </template>
@@ -48,6 +50,8 @@ interface Emits {
 }
 const props = defineProps<Props>();
 const emits = defineEmits<Emits>();
+
+const sequenceElm = ref<HTMLDivElement | null>(null);
 
 const work = computed(() => props.work);
 const workStamps = computed(() => {
